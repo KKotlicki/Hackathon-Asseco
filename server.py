adae25e968c285bfbe4dfadf15f3e25527a3e37e
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2021-10-22 22:43:09 +0200

    CORS

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 from flask import Flask, jsonify, request
-from flask_restful import reqparse, abort, Api, Resource
+from flask_restful import Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 api = Api(app)
 
 class Transaction(Resource):
     def post(self):
         json_data = request.get_json(force=True)
         money = json_data['money']
-
+        print('hey')
         return jsonify(money)
 
-api.add_resource(Transaction, '/transaction')
+api.add_resource(Transaction, '/api')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5444 ,debug=True)
