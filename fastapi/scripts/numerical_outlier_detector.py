d0e96d7122b83cbdab3834a1e20bb72c1046b1ad
kotlik <konrad.kotlicki@gmail.com>
2021-10-23 17:00:00 +0200

    Update numerical_outlier_detector.py

--- fastapi/scripts/numerical_outlier_detector.py
+++ fastapi/scripts/numerical_outlier_detector.py
@@ -3,18 +3,18 @@ import numpy as np
 
 
 class DataModelNum:
-    def __init__(self, df_train):
+    def __init__(self, arr_train):
         self.pred = None
         self.preds = None
         self.outliers_rate = None
         self.model = IsolationForest(max_samples=1000, random_state=1, contamination='auto')
-        self.update_model(df_train)
+        self.update_model(arr_train)
 
     def __repr__(self):
         return str(self.preds)
 
-    def update_model(self, df_train):
-        self.preds = self.model.fit_predict(df_train)
+    def update_model(self, arr_train):
+        self.preds = self.model.fit_predict(arr_train)
         unique, counts = np.unique(self.preds, return_counts=True)
         preds_dict = dict(zip(unique, counts))
         self.outliers_rate = preds_dict[-1]/(preds_dict[-1]+preds_dict[1])
