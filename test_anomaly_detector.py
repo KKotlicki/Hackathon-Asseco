3ab0d90d1e3c9cf582d8ea8c6900ac78584bb541
kotlik <konrad.kotlicki@gmail.com>
2021-10-23 15:39:46 +0200

    Update test_anomaly_detector.py

--- test_anomaly_detector.py
+++ test_anomaly_detector.py
@@ -11,11 +11,11 @@ def test_random(data, record):
 
 if __name__ == "__main__":
     record = np.array([[2600, 7, 32, 48]])
-    data = np.random.randn(500000, 4)
-    print(data)
-    data[:, 0] = data[:, 0] * 30000 + 20000
-    print(data)
-    data[:, 1] = data[:, 1] * 2 + 6
-    data[:, 2] = data[:, 2] * 15 + 35
-    data[:, 3] = data[:, 3] * 20 + 50
-    test_random(data, record)
+    data_num = np.random.randn(500000, 4)
+    data_num[:, 0] = data_num[:, 0] * 30000 + 20000
+    data_num[:, 1] = data_num[:, 1] * 2 + 6
+    data_num[:, 2] = data_num[:, 2] * 15 + 35
+    data_num[:, 3] = data_num[:, 3] * 20 + 50
+    test_random(data_num, record)
+
+    data_cat = None
