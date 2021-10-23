f8f20337fa42777362849ef198b25dfeb130ed6a
kotlik <konrad.kotlicki@gmail.com>
2021-10-23 16:50:26 +0200

    Update numerical_outlier_detector.py

--- fastapi/scripts/numerical_outlier_detector.py
+++ fastapi/scripts/numerical_outlier_detector.py
@@ -13,8 +13,8 @@ class DataModelNum:
     def __repr__(self):
         return str(self.preds)
 
-    def update_model(self, df_training):
-        self.preds = self.model.fit_predict(df_training)
+    def update_model(self, df_train):
+        self.preds = self.model.fit_predict(df_train)
         unique, counts = np.unique(self.preds, return_counts=True)
         preds_dict = dict(zip(unique, counts))
         self.outliers_rate = preds_dict[-1]/(preds_dict[-1]+preds_dict[1])
