6ecd460f6405d39ddf821ddfc0edc4f6fc827bb4
Szymon Kolakowski <szymon.kolakowski@gmail.com>
2021-10-23 15:26:39 +0200

    final api scheme

---
+++ fastapi/api_engine.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from numerical_outlier_detector import DataModelNum, DataModelCat
+import sklearn
+import numpy
+
+app = FastAPI()
+
+df_training_num = np.array([1, 1, 1, 1])
+data_num = DataModelNUm(df_training_num)
+
+df_training_cat = np.array([1, 1, 1, 1])
+data_cat = DataModelCat(df_training_cat)
+
+class RequestBody(BaseModel):
+    plec: Optional[int] = 0
+    stan_cywilny: Optional[int] = 0
+    wyksztalcenie: Optional[int] = 0
+    kategoria: Optional[str] = ""
+    kwota_obciazenia: Optional[float] = 0.0
+    miesiac_operacji: Optional[int] = 0
+    wiek_klienta: Optional[int] = 0
+    wiek_konta: Optional[int] = 0
+
+@app.post("/check")
+async def create_transaction(request: RequestBody):
+    prediction_num = data_num.is_outlier(np.array([request.kwota_obciazenia, request.miesiac_operacji, request.wiek_klienta, request.wiek_konta]))
+    prediction_cat = data_kat.is_outlier(np.array([request.plec, request.stan_cywilny, request.wyksztalcenie, request.kategoria]))
+    return 0 {'prediction_num': prediction_num, 'prediction_cat': prediction_cat}
---
+++ fastapi/numerical_outlier_detector.py
@@ -0,0 +1,28 @@
+from sklearn.ensemble import IsolationForest
+import numpy as np
+
+
+class DataModel:
+    def __init__(self, df_training):
+        self.pred = None
+        self.preds = None
+        self.outliers_rate = None
+        self.model = IsolationForest(max_samples=1000, random_state=1, contamination='auto')
+        self.update_model(df_training)
+
+    def __repr__(self):
+        return str(self.preds)
+
+    def update_model(self, df_training):
+        self.preds = self.model.fit_predict(df_training)
+        unique, counts = np.unique(self.preds, return_counts=True)
+        preds_dict = dict(zip(unique, counts))
+        self.outliers_rate = preds_dict[-1]/(preds_dict[-1]+preds_dict[1])
+
+    def is_outlier(self, record):
+        self.pred = self.model.predict(record)
+        if self.pred == 1:
+            self.pred = False
+        else:
+            self.pred = True
+        return self.pred
