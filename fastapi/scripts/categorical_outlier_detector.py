9b60a5a40292fc757694f3e2878ddf02b96a2914
szkolakowski <86662946+szkolakowski@users.noreply.github.com>
2021-10-23 18:07:18 +0200

    update categorical

--- fastapi/scripts/categorical_outlier_detector.py
+++ fastapi/scripts/categorical_outlier_detector.py
@@ -7,7 +7,7 @@ class DataModelCat:
         self.pred = None
         self.preds = None
         self.outliers_rate = None
-        self.cat_cols = ['plec', 'stan_cywilny', 'wykształcenie', 'kategoria']
+        self.cat_cols = ['PLEC_2', 'STAN_CYWILNY_2', 'WYKSZTALCENIE_2', 'KATEGORIA_']
         self.model = TrainOutlier(95, self.cat_cols)
         self.update_model(df_train)
 
@@ -15,11 +15,14 @@ class DataModelCat:
         return str(self.preds)
 
     def update_model(self, df_train):
-        self.preds = self.model.train(df_train)
+        self.model.train(df_train)
+        self.preds = PredictOutlier(self.model, df_train).scores()
+        print(type(self.preds))
+        print(self.preds)
         unique, counts = np.unique(self.preds, return_counts=True)
         preds_dict = dict(zip(unique, counts))
         self.outliers_rate = preds_dict[-1]/(preds_dict[-1]+preds_dict[1])
 
     def is_outlier(self, record):
-        self.pred = float(PredictOutlier(self.preds, record))
+        self.pred = PredictOutlier(self.model, record).scores()
         return self.pred
